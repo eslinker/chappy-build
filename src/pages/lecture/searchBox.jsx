c3261ea35b3bdd9fa6aaf7c9fd07a12e4ed6455c
Eunsik Park <eslinker@lineus.kr>
2023-12-19 20:36:54 +0900

    채피 대시보드 초기

--- src/pages/lecture/searchBox.jsx
+++ src/pages/lecture/searchBox.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 function SearchBox(props) {
     return (
-        <div className='w-100 mt-4'>
+        <div className='w-100 mt-4 search-block'>
+            <i className="bi bi-search input-search"></i>
             <input type='text' placeholder='과목명 검색' className='w-100 search-box'/>
         </div>
     );
