ba881c29e20ece1ee09e275e1cd25106810de243
Eunsik Park <eslinker@lineus.kr>
2023-12-20 12:45:25 +0900

    Initial commit

--- src/pages/lecture/searchBox.jsx
+++ src/pages/lecture/searchBox.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+
+const focusHandler = () => {
+    const focusIcon = document.getElementById('searchIcon');
+
+}
 function SearchBox(props) {
     return (
         <div className='w-100 mt-4 search-block'>
-            <i className="bi bi-search input-search"></i>
-            <input type='text' placeholder='과목명 검색' className='w-100 search-box'/>
+            <i className="bi bi-search input-search" id='searchIcon'></i>
+            <input type='text' placeholder='과목명 검색' className='w-100 search-box' onClick={focusHandler}/>
         </div>
     );
 
